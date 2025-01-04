0016f57f3a81d23e2fd0915e1ebecdaa088f69ea
username <wecp-dummy@gmail.com>
2025-01-04 06:50:08 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/service/impl/TeamServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/TeamServiceImplJpa.java
@@ -1,36 +1,34 @@
 package com.wecp.progressive.service.impl;
-
+ 
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
+ 
+import com.wecp.progressive.dao.TeamDAO;
 import com.wecp.progressive.entity.Team;
-import com.wecp.progressive.repository.TeamRepository;
 import com.wecp.progressive.service.TeamService;
-
+ 
 public class TeamServiceImplJpa implements TeamService  {
-
-    @Autowired
-TeamRepository teamRepository;
- @Override
-    public List<Team> getAllTeams() {
-       return teamRepository.findAll();
+    private TeamDAO teamDAO;
+   
+ 
+    public TeamServiceImplJpa(TeamDAO teamDAO) {
+        this.teamDAO = teamDAO;
     }
-
+ 
+    @Override
+    public List<Team> getAllTeams() {
+        return null;
+       
+    }
+ 
     @Override
     public int addTeam(Team team) {
-      teamRepository.save(team);
-        return team.getTeamId();
+        return -1;
     }
-
+ 
     @Override
     public List<Team> getAllTeamsSortedByName() {
         return List.of();
     }
-
-@Override
-public void emptyArrayList(){
-    TeamService.super.emptyArrayList();
-}
-
+   
+ 
 }
